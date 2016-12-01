8b04f640d53040a1cc019eb11bd4aaf00a62f1cf
Aradap_Programmer <Aradap_Programmer>
2016-12-01 13:04:20 +0300

    fix js

---
+++ WebContent/resources/js/navbar.js
@@ -0,0 +1,12 @@
+ $(document).ready(function () {
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+         return this.href == url;
+    }).parent().addClass('active').parent().parent().addClass('active').parent().parent().addClass('active');
+     
+});
+ 
+
+ 
+ 
