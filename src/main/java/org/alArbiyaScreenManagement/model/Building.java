7fcfeb7770e8d5ed6087498e1e57605de7755713
Aradap_Programmer <Aradap_Programmer>
2016-12-25 12:25:09 +0300

    fix player room

---
+++ src/main/java/org/alArbiyaScreenManagement/model/Building.java
@@ -0,0 +1,155 @@
+package org.alArbiyaScreenManagement.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+@Table(name="BUILDING")
+public class Building {
+
+	
+	@Id @GeneratedValue 
+	@Column(name="BUILDING_ID")
+	private long id;
+	
+	@Column(name="BUILDING_NAME") 
+	private String buildingName;
+	
+	@Column(name="BUILDING_CODE", unique=true) 
+	private String buildingCode;
+	
+	@Column(name="BUILDING_PHONENUMBER") 
+	private String buildingPhoneNumber;
+	
+	@Column(name="BUILDING_EMAIL") 
+	private String buildingEmail;
+	
+	@Column(name="BUILDING_TOTALFLOOR") 
+	private long buildingTotalFloor;
+	
+	@Column(name="BUILDING_DESCRIPTION") 
+	private String buildingDescription;
+	
+	@Column(name="BUILDING_STATUS") 
+	private String buildingStatus;
+	
+	@JsonBackReference
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="BRANCH_ID")
+	private Branch branch;
+	
+	@JsonIgnore 
+	@JsonManagedReference
+	@OneToMany(mappedBy="building",cascade={CascadeType.MERGE}, fetch=FetchType.EAGER)
+	private List<Floor> floors;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getBuildingName() {
+		return buildingName;
+	}
+
+	public void setBuildingName(String buildingName) {
+		this.buildingName = buildingName;
+	}
+
+	public String getBuildingCode() {
+		return buildingCode;
+	}
+
+	public void setBuildingCode(String buildingCode) {
+		this.buildingCode = buildingCode;
+	}
+
+	public String getBuildingPhoneNumber() {
+		return buildingPhoneNumber;
+	}
+
+	public void setBuildingPhoneNumber(String buildingPhoneNumber) {
+		this.buildingPhoneNumber = buildingPhoneNumber;
+	}
+
+	public String getBuildingEmail() {
+		return buildingEmail;
+	}
+
+	public void setBuildingEmail(String buildingEmail) {
+		this.buildingEmail = buildingEmail;
+	}
+
+	public long getBuildingTotalFloor() {
+		return buildingTotalFloor;
+	}
+
+	public void setBuildingTotalFloor(long buildingTotalFloor) {
+		this.buildingTotalFloor = buildingTotalFloor;
+	}
+
+	public String getBuildingDescription() {
+		return buildingDescription;
+	}
+
+	public void setBuildingDescription(String buildingDescription) {
+		this.buildingDescription = buildingDescription;
+	}
+
+	public String getBuildingStatus() {
+		return buildingStatus;
+	}
+
+	public void setBuildingStatus(String buildingStatus) {
+		this.buildingStatus = buildingStatus;
+	}
+	
+	public Branch getBranch() {
+		return branch;
+	}
+
+	public void setBranch(Branch branch) {
+		this.branch = branch;
+        if (branch.getBuildings()!=null && !branch.getBuildings().contains(this)) {
+        	branch.getBuildings().add(this);
+        }
+	}
+	 
+	
+	public List<Floor> getFloors() {
+		return floors;
+	}
+
+	public void addFloors(Floor floor) {
+        this.floors.add(floor);
+        if (floor.getBuilding() != this) {
+        	floor.setBuilding(this);
+        }
+    }
+	
+	public void setFloors(List<Floor> floors) {
+		this.floors = floors;
+	} 
+	
+	
+	
+	
+}
+
---
+++ src/main/java/org/alArbiyaScreenManagement/model/Floor.java
@@ -0,0 +1,125 @@
+package org.alArbiyaScreenManagement.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+@Entity
+@Table(name="FLOOR")
+public class Floor {
+
+	@Id @GeneratedValue 
+	@Column(name="FLOOR_ID")
+	private long id;
+	
+	@Column(name="FLOOR_NAME") 
+	private String floorName;
+	
+	@Column(name="FLOOR_CODE", unique=true) 
+	private String floorCode;
+	
+	@Column(name="FLOOR_TOTALROOM") 
+	private long floorTotalRoom;
+	
+	@Column(name="FLOOR_DESCRIPTION") 
+	private String floorDescription;
+	
+	@Column(name="FLOOR_STATUS") 
+	private String floorStatus;
+
+	@JsonBackReference
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="BUILDING_ID")
+	private Building building;
+	
+	@JsonIgnore 
+	@OneToMany(mappedBy="floor", cascade={CascadeType.MERGE}, fetch=FetchType.EAGER)
+	private List<Room> rooms;
+
+	public List<Room> getRooms() {
+		return rooms;
+	}
+	
+	public void addRoom(Room room) {
+        this.rooms.add(room);
+        if (room.getFloor() != this) {
+        	room.setFloor(this);
+        }
+    }
+
+	public void setRooms(List<Room> rooms) {
+		this.rooms = rooms;
+	}
+
+	public String getFloorStatus() {
+		return floorStatus;
+	}
+
+	public void setFloorStatus(String floorStatus) {
+		this.floorStatus = floorStatus;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFloorName() {
+		return floorName;
+	}
+
+	public void setFloorName(String floorName) {
+		this.floorName = floorName;
+	}
+
+	public String getFloorCode() {
+		return floorCode;
+	}
+
+	public void setFloorCode(String floorCode) {
+		this.floorCode = floorCode;
+	}
+
+	public long getFloorTotalRoom() {
+		return floorTotalRoom;
+	}
+
+	public void setFloorTotalRoom(long floorTotalRoom) {
+		this.floorTotalRoom = floorTotalRoom;
+	}
+
+	public String getFloorDescription() {
+		return floorDescription;
+	}
+
+	public void setFloorDescription(String floorDescription) {
+		this.floorDescription = floorDescription;
+	}
+
+	public Building getBuilding() {
+		return building;
+	}
+
+	public void setBuilding(Building building) {
+		this.building = building;
+        if (building.getFloors()!=null && !building.getFloors().contains(this)) {
+        	building.getFloors().add(this);
+        }
+	}
+}
